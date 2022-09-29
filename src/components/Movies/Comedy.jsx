45b08f294ecb12af4394c4719dad6aa1cc0a787c
Mariah <mariahschock@gmail.com>
2022-09-28 17:58:28 -0700

    added movies to components

--- src/components/Movies/Comedy.jsx
+++ src/components/Movies/Comedy.jsx
@@ -1,6 +1,13 @@
 
 export default function Comedy() {
   return (
-    <div>Comedy</div>
+    <div>
+      <h1>Comedy Movies</h1>
+      <ul>
+        <li>Wedding Crashers</li>
+        <li>Freaky Friday</li>
+        <li>No Way Home</li>
+      </ul>
+    </div>
   );
 }
--- src/components/Movies/Drama.jsx
+++ src/components/Movies/Drama.jsx
@@ -1,6 +1,13 @@
 
 export default function Drama() {
   return (
-    <div>Drama</div>
+    <div>
+      <h1>Drama Movies</h1>
+      <ul>
+        <li>Once Upon a Time in Hollywood</li>
+        <li>Elvis</li>
+        <li>Titanic</li>
+      </ul>
+    </div>
   );
 }
